a4b978d5696fad0d28d5e9c4a311fb804d8dc3f2
shava04 <shavarsh004@gmail.com>
2017-12-07 20:15:57 +0300

    Settings added

---
+++ Classes/Settings.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public class Settings
+    {
+        private int time;
+        public int Time
+        {
+            get { return time; }
+            set { time = value; }
+        }
+        private int level;
+        public int Level
+        {
+            get { return level; }
+            set { level = value; }
+        }
+        public Settings(int tm, int l)
+        {
+            this.Time = tm;
+            this.level = l;
+        }
+        public Settings()
+        {
+            this.time = 90;
+            this.level = 1;
+        }
+    }
+}
--- Football Championship System/MainWindow.xaml.cs
+++ Football Championship System/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Classes;
 
 namespace Football_Championship_System
 {
@@ -48,5 +49,33 @@ namespace Football_Championship_System
             GridEnter.Visibility = Visibility.Hidden;
             GridRegistration.Visibility = Visibility.Visible;
         }
+
+        private void ButtonSettingsBack_Click(object sender, RoutedEventArgs e)
+        {
+            GridSettings.Visibility = Visibility.Hidden;
+            GridEnter.Visibility = Visibility.Visible;
+        }
+
+        private void ButtonSettingsOK_Click(object sender, RoutedEventArgs e)
+        {
+            //logic for settings
+            if (RadioButton60sec.IsChecked==false && RadioButton90sec.IsChecked==false && RadioButton180sec.IsChecked==false)
+            {
+                MessageBox.Show("Choose one of the possible time", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            if (RadioButton1Level.IsChecked==false && RadioButton2Level.IsChecked==false && RadioButton3Level.IsChecked==false && RadioButton4Level.IsChecked==false)
+            {
+                MessageBox.Show("Choose one of the possible level", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            Settings settings = new Settings();
+            GridSettings.Visibility = Visibility.Hidden;
+            GridEnter.Visibility = Visibility.Visible;
+        }
+
+        private void ButtonSettings_Click(object sender, RoutedEventArgs e)
+        {
+            GridEnter.Visibility = Visibility.Hidden;
+            GridSettings.Visibility = Visibility.Visible;
+        }   
     }
 }
