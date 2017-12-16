aa9941587167099ae42797b32e7c30f802ce40d3
misha <goihmanm@mail.ru>
2017-12-16 23:04:16 +0300

    Added: emailsender method created

---
+++ ServiceClasses/EmailSender.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceClasses
+{
+    class EmailSender
+    {
+        public void SendEmail(string email, int score)
+        {
+            MailAddress fromMailAddress = new MailAddress("FCSapi2017@gmail.com", "FCS Team");
+            MailAddress ToAdress = new MailAddress(email, "Player");
+            using (MailMessage mailMessage = new MailMessage(fromMailAddress, ToAdress))
+            using (SmtpClient smtpclient = new SmtpClient())
+            {
+                mailMessage.Subject = "Поздравляем, вы прошли чемпионат!";
+                mailMessage.Body = "Вы набрали "+score+"очков!";
+
+                smtpclient.Host = "smtp.gmail.com";
+                smtpclient.Port = 587;
+                smtpclient.EnableSsl = true;
+                smtpclient.DeliveryMethod = SmtpDeliveryMethod.Network;
+                smtpclient.UseDefaultCredentials = false;
+                smtpclient.Credentials = new NetworkCredential(fromMailAddress.Address, "apifcs2017");
+
+                smtpclient.Send(mailMessage);
+            }
+    }
+}
