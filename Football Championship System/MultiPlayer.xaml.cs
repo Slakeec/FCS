2ec5444bcb2c1c8df334ffa201724ecce10156ed
shava04 <shavarsh004@gmail.com>
2017-12-16 21:44:02 +0300

    MultiPlayer window added

---
+++ Football Championship System/MultiPlayer.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Football_Game;
+using GameFootball;
+
+namespace Football_Championship_System
+{
+    /// <summary>
+    /// Interaction logic for MultiPlayer.xaml
+    /// </summary>
+    public partial class MultiPlayer : Window
+    {
+        public MultiPlayer()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonBack_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void ButtonOK_Click(object sender, RoutedEventArgs e)
+        {
+            FootballGameForm f = new FootballGameForm();
+            
+        }
+    }
+}
